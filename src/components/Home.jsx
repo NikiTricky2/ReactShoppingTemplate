8b086c8598186841d2f39861dcb578119e743847
NIkiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-05-18 13:52:22 +0300

    Made the product image a carousell

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to <span className="text-primary">ShopShop</span>!</h1>
-            <Product imgSrc="/images/keyboard.webp" title="Keyboard" desc="Adipisicing nostrud in exercitation eu tempor adipisicing. Dolor fugiat quis eiusmod eu incididunt excepteur ex id consectetur adipisicing officia excepteur. Cupidatat in consequat est nostrud non do reprehenderit adipisicing sunt deserunt irure do magna mollit." />
+            <Product id="0" imgSrcs={["/images/keyboard.webp"]} title="Keyboard" desc="Adipisicing nostrud in exercitation eu tempor adipisicing. Dolor fugiat quis eiusmod eu incididunt excepteur ex id consectetur adipisicing officia excepteur. Cupidatat in consequat est nostrud non do reprehenderit adipisicing sunt deserunt irure do magna mollit." />
         </div>
     );
   }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,29 @@
 function Product(props) {
+    var a = [];
+    for (var i = 0; i < props.imgSrcs.length - 1; i++) {
+        a.push(i);
+    }
     return (
         <div className="card" style={{width: '18rem'}}>
-        <img src={props.imgSrc} className="card-img-top" alt={props.title} />
+        <div id={"id" + props.id + "car"} class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div className="carousel-indicators">
+            <button style={{border: 'none', background: 'gray'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide-to="0" className="active" aria-current="true"></button>
+            {a.map((value, index) => {
+                return <button style={{border: 'none', background: 'gray'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide-to={index}></button>
+            })}
+        </div>
+        <div className="carousel-inner">
+        {props.imgSrcs.map((value, index) => {
+            return <div className="carousel-item active"> <img src={value} className="d-block w-100" alt={props.title} /> </div>
+        })}
+        </div>
+        <button className="carousel-control-prev" style={{border: 'none', background: 'none', filter: 'invert(50%)'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button className="carousel-control-next" style={{border: 'none', background: 'none', filter: 'invert(50%)'}}  type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
+        </div>
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.desc}</p>
