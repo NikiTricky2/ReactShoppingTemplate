e9e726a09269066899be9e530bcb6c7793e50e26
NIkiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-05-18 11:43:41 +0300

    Add navbar and bootstrap

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
+import './css/bootstrap.min.css';
+import './css/custom.css'
+
 ReactDOM.render(( 
     <BrowserRouter >
-    <App / >
+    <App />
     </BrowserRouter>
 ), document.getElementById('root'));
