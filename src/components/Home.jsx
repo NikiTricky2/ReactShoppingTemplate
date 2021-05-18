edffe681d5eb80d0cdb1dbf1545b2426bc660532
NIkiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-05-18 12:46:39 +0300

    Added products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,11 @@
+import Product from './Product';
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Welcome to <span className="text-primary">ShopShop</span>!</h1>
+            <Product imgSrc="/images/keyboard.webp" title="Keyboard" desc="Adipisicing nostrud in exercitation eu tempor adipisicing. Dolor fugiat quis eiusmod eu incididunt excepteur ex id consectetur adipisicing officia excepteur. Cupidatat in consequat est nostrud non do reprehenderit adipisicing sunt deserunt irure do magna mollit." />
+        </div>
     );
   }
   
