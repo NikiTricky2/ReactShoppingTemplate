5f7ff56d6ccdde793dbc815d32c59aff17454e2b
NIkiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-05-18 15:59:14 +0300

    Fix npm test

--- src/index.test.js
+++ src/index.test.js
@@ -9,6 +9,6 @@ test('Test', () => {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script>
         </BrowserRouter>
     );
-  const linkElement = screen.queryByTest("Аpp");
+  const linkElement = screen.queryByTestId("Аpp");
   expect(linkElement).toBeDefined();
 });
