4bdab106a3ae6239ed6300388502000d930b3e7e
NIkiTricky <63540250+NikiTricky2@users.noreply.github.com>
2021-05-25 12:14:07 +0300

    Removed product carousel

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,29 +1,7 @@
 function Product(props) {
-    var a = [];
-    for (var i = 0; i < props.imgSrcs.length - 1; i++) {
-        a.push(i);
-    }
     return (
-        <div className="card" style={{width: '18rem'}}>
-        <div id={"id" + props.id + "car"} className="carousel carousel-dark slide" data-bs-ride="carousel">
-        <div className="carousel-indicators">
-            <button style={{border: 'none', background: 'gray'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide-to="0" className="active" aria-current="true"></button>
-            {a.map((value, index) => {
-                return <button key={value} style={{border: 'none', background: 'gray'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide-to={index}></button>
-            })}
-        </div>
-        <div className="carousel-inner">
-        {props.imgSrcs.map((value, index) => {
-            return <div className="carousel-item active" key={value}> <img src={value} className="d-block w-100" alt={props.title} /> </div>
-        })}
-        </div>
-        <button className="carousel-control-prev" style={{border: 'none', background: 'none', filter: 'invert(50%)'}} type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        </button>
-        <button className="carousel-control-next" style={{border: 'none', background: 'none', filter: 'invert(50%)'}}  type="button" data-bs-target={"#id" + props.id + "car"} data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        </button>
-        </div>
+        <div className="card" style={{width: '18rem'}}> 
+        <img src={props.imgSrc} className="card-img-top" alt={props.title} />
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.desc}</p>
--- src/index.test.js
+++ src/index.test.js
@@ -3,12 +3,12 @@ import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 
 test('Test', () => {
-  render(
-        <BrowserRouter >
-        <App />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script>
+    render( 
+	<BrowserRouter >
+          <App />
+          <script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity = "sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"></script> 
         </BrowserRouter>
     );
-  const linkElement = screen.queryByTestId("Аpp");
-  expect(linkElement).toBeDefined();
+    const linkElement = screen.queryByTestId("Аpp");
+    expect(linkElement).toBeDefined();
 });
